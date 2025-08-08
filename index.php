64a956adf2b3db6dcf44e9ef89e640d51c308fc3
praveenkumawat1 <kumawatpraveen276@gmail.com>
2025-08-08 15:58:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+    <script module="" src="script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Anton&family=Bebas+Neue&family=Bitcount+Prop+Single:wght@100..900&family=Bungee&family=Bungee+Spice&family=Cabin:ital,wght@0,400..700;1,400..700&family=Changa+One:ital@0;1&family=Exile&family=Hammersmith+One&family=Keania+One&family=League+Spartan&family=Libertinus+Mono&family=Lora:ital,wght@0,400..700;1,400..700&family=Pacifico&family=Playwrite+IT+Moderna:wght@100..400&family=Playwrite+MX+Guides&family=Playwrite+VN+Guides&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rowdies:wght@300;400;700&family=TikTok+Sans:opsz,wght@12..36,300..900&family=WindSong:wght@400;500&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <h1>Buzz</h1>
+                <h1>Beverages</h1>
+            </div>
+            <div class="navlist">
+                <ul>
+                    <li><a href="#">SHOP NOW</a></li>
+                    <li><a href="#">OUR STORY</a></li>
+                    <li><a href="#">FIND US</a></li>
+                    <li><a href="#">BLOG</a></li>
+                </ul>
+            </div>
+            <div class="cart">
+                <a href="#">LOGIN</a>
+                <a href="#">CART</a>
+            </div>
+        </nav>
+        <div class="home">
+            <h1>eur <span>o</span> pa</h1>
+        </div>
+        <div class="home-img">
+            <img src="Assets/drink.png" alt="">
+            <img src="Assets/badage.png" alt="" id="badage">
+        </div>
+    </header>
+    <main>
+        <div class="ordernow">
+            <p>Tastes like the soda you grew up sipping, but with the added benefit of microbiome and digestive
+                health support today.</p>
+            <a href="#">order now</a>
+        </div>
+
+    </main>
+    <footer></footer>
+</body>
+
+</html>
